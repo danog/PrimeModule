b23b3b626241f05596181dfb1d5590209534325a
Daniil Gentili <daniil.gentili.dg@gmail.com>
2017-02-21 19:37:13 +0000

    Apply fixes from StyleCI

--- tests/testing.php
+++ tests/testing.php
@@ -2,74 +2,82 @@
 <?php
 
 require 'vendor/autoload.php';
-function get_time($callable, $param) {
+function get_time($callable, $param)
+{
     $a = microtime(true);
     $result = $callable($param);
     $time = microtime(true) - $a;
-    if (is_array($result)) $result = json_encode($result);
+    if (is_array($result)) {
+        $result = json_encode($result);
+    }
+
     return [$time, $result];
 }
-function test($n) {
-    $init = '| '.str_pad('result', strlen($n), " ", STR_PAD_RIGHT).' |  type       |         time         |';
-    echo '|'.str_pad('', strlen($init)-2, "-", STR_PAD_RIGHT).'|'.PHP_EOL;
+function test($n)
+{
+    $init = '| '.str_pad('result', strlen($n), ' ', STR_PAD_RIGHT).' |  type       |         time         |';
+    echo '|'.str_pad('', strlen($init) - 2, '-', STR_PAD_RIGHT).'|'.PHP_EOL;
 
-    echo '|'.str_pad('Multiple factorization of '.$n, strlen($init)-2, " ", STR_PAD_BOTH).'|'.PHP_EOL;
-    echo '|'.str_pad('', strlen($init)-2, "_", STR_PAD_RIGHT).'|'.PHP_EOL;
+    echo '|'.str_pad('Multiple factorization of '.$n, strlen($init) - 2, ' ', STR_PAD_BOTH).'|'.PHP_EOL;
+    echo '|'.str_pad('', strlen($init) - 2, '_', STR_PAD_RIGHT).'|'.PHP_EOL;
     echo $init.PHP_EOL;
     list($time, $result) = get_time(['\danog\PrimeModule', 'python_alt'], $n, true);
     $GLOBALS['medium']['python_alt'] += $time;
-    echo '| '.str_pad($result, 6, " ", STR_PAD_RIGHT).' |  python alt | '.str_pad($time, 20, " ", STR_PAD_RIGHT).' |'.PHP_EOL;
+    echo '| '.str_pad($result, 6, ' ', STR_PAD_RIGHT).' |  python alt | '.str_pad($time, 20, ' ', STR_PAD_RIGHT).' |'.PHP_EOL;
     list($time, $result) = get_time(['\danog\PrimeModule', 'python'], $n);
     $GLOBALS['medium']['python'] += $time;
-    echo '| '.str_pad($result, 6, " ", STR_PAD_RIGHT).' |  wolfram    | '.str_pad($time, 20, " ", STR_PAD_RIGHT).' |'.PHP_EOL;
+    echo '| '.str_pad($result, 6, ' ', STR_PAD_RIGHT).' |  wolfram    | '.str_pad($time, 20, ' ', STR_PAD_RIGHT).' |'.PHP_EOL;
     list($time, $result) = get_time(['\danog\PrimeModule', 'native'], $n);
     $GLOBALS['medium']['native'] += $time;
-    echo '| '.str_pad($result, 6, " ", STR_PAD_RIGHT).' |  python     | '.str_pad($time, 20, " ", STR_PAD_RIGHT).' |'.PHP_EOL;
+    echo '| '.str_pad($result, 6, ' ', STR_PAD_RIGHT).' |  python     | '.str_pad($time, 20, ' ', STR_PAD_RIGHT).' |'.PHP_EOL;
     list($time, $result) = get_time(['\danog\PrimeModule', 'wolfram'], $n);
     $GLOBALS['medium']['wolfram'] += $time;
-    echo '| '.str_pad($result, 6, " ", STR_PAD_RIGHT).' |  native     | '.str_pad($time, 20, " ", STR_PAD_RIGHT).' |'.PHP_EOL;
+    echo '| '.str_pad($result, 6, ' ', STR_PAD_RIGHT).' |  native     | '.str_pad($time, 20, ' ', STR_PAD_RIGHT).' |'.PHP_EOL;
     //echo '| '.str_pad($n, 6, " ", STR_PAD_RIGHT).' |  auto       | '.str_pad(get_time(['\danog\PrimeModule', 'auto'], $n), 20, " ", STR_PAD_RIGHT).' |'.PHP_EOL;
-    echo '|'.str_pad('', strlen($init)-2, "-", STR_PAD_RIGHT).'|'.PHP_EOL.PHP_EOL;
-
+    echo '|'.str_pad('', strlen($init) - 2, '-', STR_PAD_RIGHT).'|'.PHP_EOL.PHP_EOL;
 }
-function test_single($n, $messy = false) {
-    $init = '| '.str_pad('result', strlen($n), " ", STR_PAD_RIGHT).' |  type       |         time         |';
-    echo '|'.str_pad('', strlen($init)-2, "-", STR_PAD_RIGHT).'|'.PHP_EOL;
-    echo '|'.str_pad('Single factorization of '.$n, strlen($init)-2, " ", STR_PAD_BOTH).'|'.PHP_EOL;
-    echo '|'.str_pad('', strlen($init)-2, "_", STR_PAD_RIGHT).'|'.PHP_EOL;
+function test_single($n, $messy = false)
+{
+    $init = '| '.str_pad('result', strlen($n), ' ', STR_PAD_RIGHT).' |  type       |         time         |';
+    echo '|'.str_pad('', strlen($init) - 2, '-', STR_PAD_RIGHT).'|'.PHP_EOL;
+    echo '|'.str_pad('Single factorization of '.$n, strlen($init) - 2, ' ', STR_PAD_BOTH).'|'.PHP_EOL;
+    echo '|'.str_pad('', strlen($init) - 2, '_', STR_PAD_RIGHT).'|'.PHP_EOL;
     echo $init.PHP_EOL;
 
     list($time, $result) = get_time(['\danog\PrimeModule', 'python_single_alt'], $n);
     $GLOBALS['medium']['python_alt'] += $time;
-    echo '| '.str_pad($result, strlen($n), " ", STR_PAD_RIGHT).' |  python alt | '.str_pad($time, 20, " ", STR_PAD_RIGHT).' |'.PHP_EOL;
+    echo '| '.str_pad($result, strlen($n), ' ', STR_PAD_RIGHT).' |  python alt | '.str_pad($time, 20, ' ', STR_PAD_RIGHT).' |'.PHP_EOL;
     if (!$messy) {
         list($time, $result) = get_time(['\danog\PrimeModule', 'python_single'], $n);
         $GLOBALS['medium']['python'] += $time;
-        echo '| '.str_pad($result, strlen($n), " ", STR_PAD_RIGHT).' |  python     | '.str_pad($time, 20, " ", STR_PAD_RIGHT).' |'.PHP_EOL;
+        echo '| '.str_pad($result, strlen($n), ' ', STR_PAD_RIGHT).' |  python     | '.str_pad($time, 20, ' ', STR_PAD_RIGHT).' |'.PHP_EOL;
     }
-    
+
     list($time, $result) = get_time(['\danog\PrimeModule', 'native_single'], $n);
     $GLOBALS['medium']['native'] += $time;
-    echo '| '.str_pad($result, strlen($n), " ", STR_PAD_RIGHT).' |  native     | '.str_pad($time, 20, " ", STR_PAD_RIGHT).' |'.PHP_EOL;
+    echo '| '.str_pad($result, strlen($n), ' ', STR_PAD_RIGHT).' |  native     | '.str_pad($time, 20, ' ', STR_PAD_RIGHT).' |'.PHP_EOL;
     list($time, $result) = get_time(['\danog\PrimeModule', 'wolfram_single'], $n);
     $GLOBALS['medium']['wolfram'] += $time;
-    echo '| '.str_pad($result, strlen($n), " ", STR_PAD_RIGHT).' |  wolfram    | '.str_pad($time, 20, " ", STR_PAD_RIGHT).' |'.PHP_EOL;
-    
-    echo '|'.str_pad('', strlen($init)-2, "-", STR_PAD_RIGHT).'|'.PHP_EOL.PHP_EOL;
+    echo '| '.str_pad($result, strlen($n), ' ', STR_PAD_RIGHT).' |  wolfram    | '.str_pad($time, 20, ' ', STR_PAD_RIGHT).' |'.PHP_EOL;
 
+    echo '|'.str_pad('', strlen($init) - 2, '-', STR_PAD_RIGHT).'|'.PHP_EOL.PHP_EOL;
 }
-function random_string($length) {
-    if (function_exists('random_bytes')) return random_bytes($length);
+function random_string($length)
+{
+    if (function_exists('random_bytes')) {
+        return random_bytes($length);
+    }
     $s = '';
     for ($x = 0; $x < $length; $x++) {
         $s .= chr(rand(0, 255));
     }
+
     return $s;
 }
-function gen_payload() {
+function gen_payload()
+{
     return pack('CPPVV', 10, 0, (int) (time() << 32), 20, 1615239032).random_string(16);
 }
-
 
 echo PHP_EOL.'----------- HUGE SEMIPRIME TESTS (100 semiprimes) ----------'.PHP_EOL;
 $GLOBALS['medium'] = ['python' => 0, 'python_alt' => 0, 'wolfram' => 0, 'native' => 0];
@@ -93,7 +101,7 @@ for ($x = 0; $x < $tot; $x++) {
 fclose($tg);
 
 foreach ($medium as $type => $total) {
-    echo $type.': total time '.$total.', medium time '.($total/$tot).PHP_EOL;
+    echo $type.': total time '.$total.', medium time '.($total / $tot).PHP_EOL;
 }
 echo PHP_EOL.'------------------- SMALL MULTIPLE FACTOR TESTS -------------------'.PHP_EOL;
 $GLOBALS['medium'] = ['python' => 0, 'python_alt' => 0, 'wolfram' => 0, 'native' => 0];
@@ -103,9 +111,8 @@ foreach ([200, 327, 35, 13589] as $multiple) {
 }
 
 foreach ($medium as $type => $total) {
-    echo $type.': total time '.$total.', medium time '.($total/4).PHP_EOL;
+    echo $type.': total time '.$total.', medium time '.($total / 4).PHP_EOL;
 }
-
 
 echo PHP_EOL.'------------------- HUGE SEMIPRIME TESTS (MESSY) ------------------'.PHP_EOL;
 $GLOBALS['medium'] = ['python' => 0, 'python_alt' => 0, 'wolfram' => 0, 'native' => 0];
@@ -114,5 +121,5 @@ foreach ($m as $messy) {
     test_single($messy, true);
 }
 foreach ($medium as $type => $total) {
-    echo $type.': total time '.$total.', medium time '.($total/count($m)).PHP_EOL;
+    echo $type.': total time '.$total.', medium time '.($total / count($m)).PHP_EOL;
 }
