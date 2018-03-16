99de2cd8d19be07bcc49a5e1f2d1dee859241834
Daniil Gentili <daniil.gentili.dg@gmail.com>
2018-03-16 12:18:27 +0000

    Apply fixes from StyleCI

--- tests/testing.php
+++ tests/testing.php
@@ -94,7 +94,6 @@ function gen_payload()
     return chr(10).str_repeat(chr(0), 8).\danog\PHP\Struct::pack('<q', (int) (time() << 32)).pack('VV', 20, 1615239032).random_string(16);
 }
 
-
 echo PHP_EOL.'----------- HUGE SEMIPRIME TESTS (100 semiprimes) ----------'.PHP_EOL;
 $GLOBALS['medium'] = ['python' => 0, 'python_alt' => 0, 'wolfram' => 0, 'native' => 0, 'native_cpp' => 0];
 $tg = fsockopen('tcp://149.154.167.51:443');
