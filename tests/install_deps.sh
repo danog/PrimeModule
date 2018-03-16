df0a4ccf9b899b6d31afbdae183d9ae6a3a9d75a
Daniil Gentili <daniil@daniil.it>
2018-03-16 13:20:36 +0100

    Improve deps installation script

--- tests/install_deps.sh
+++ tests/install_deps.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 export PATH=$PATH:~/.phpenv/versions/$(phpenv version-name)/bin
 
-cd /tmp
-git clone https://github.com/CopernicaMarketingSoftware/PHP-CPP
-cd PHP-CPP
-make -j16
-sudo make install
+cd /tmp &&
+git clone https://github.com/CopernicaMarketingSoftware/PHP-CPP &&
+cd PHP-CPP &&
+make -j16 &&
+sudo make install &&
 
-cd ../
-git clone https://github.com/danog/PrimeModule-ext
-cd PrimeModule-ext
-make -j16
-echo "extension=$PWD/primemodule.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+cd ../ &&
+git clone https://github.com/danog/PrimeModule-ext &&
+cd PrimeModule-ext &&
+make -j16 &&
+echo "extension=$PWD/primemodule.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini &&
 cd ..
 
 exit 0
