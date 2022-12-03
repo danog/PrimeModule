970feba34fe7c019f7925d7a68085117412504e4
Daniil Gentili <daniil@daniil.it>
2022-12-03 21:54:02 +0100

    PHP 8.1 support
    Release transpiled using https://phabel.io, the PHP transpiler

---
+++ ___transpiledWithPhabel.php
@@ -0,0 +1,3 @@
+<?php if (!getenv("PHABEL_INSIDE_COMPOSER")) die("danog/primemodule: This package requires transpilation using the phabel.io composer plugin.
+To use this package please run composer update, enabling execution of plugins and generation of the lockfile.
+");
